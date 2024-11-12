52b10ec43adbe765da3e6ed6c03030736038877d
pessi-v <pessimo@posteo.net>
2024-11-12 14:19:36 +0100

    add pico css and first blog route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+	import '@picocss/pico';
+	import '../app.scss';
+</script>
+
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: sveltePreprocess({
+		scss: true
+	}),
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
