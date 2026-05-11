ecded32b6ac7b56f0ac8b46a2f98a5e19cf38753
pessi-v <pessimo@posteo.net>
2026-05-11 21:49:13 +0200

    new post

---
+++ src/routes/blog/may_11_26/+page.svelte
@@ -0,0 +1,46 @@
+<div class="post">
+	<h2>A update on what I've been doing!</h2>
+	<h4>May 11, 2026</h4>
+
+	<p>
+		I haven't been updating this blog very much - it's been almost two years since I wrote a post!
+		Here's a really quick summary of what I've been up to since August 2024.
+	</p>
+
+	<ul>
+		<li>I moved back to Berlin in the spring of last year</li>
+		<li>
+			I developed a VSCode Extension, in TypeScript, in a team of three, funded by the Prototype
+			Fund in Germany. <a href="https://www.prototypefund.de/en/projects/carbonara"
+				>Here's a description of the project on the Prototype Fund page.</a
+			>
+			and <a href="https://github.com/climateandtech/carbonara">this is the git repo</a>
+		</li>
+		<li>Took a course on leadership</li>
+		<li>
+			Built <a href="https://receitas.pessi.info/">this cookbook</a> in order to learn CraftCMS/PHP,
+			and ended up learning some three.js, as well as about self-hosting and Cloudflare tunnels on the
+			side
+		</li>
+		<li>Started a <a href="https://timeliner.pessi.info/">historical timeline</a> project</li>
+		<li>
+			Completed Mastodon integration for <a href="https://github.com/pessi-v/metazine">Metazine</a>
+		</li>
+		<li>Added KokoroTTS (a modern text-to-speech model) to Metazine.</li>
+		<li>
+			Did some proof-of-concepts for <a href="https://gamepad.pessi.info/"
+				>laptop-smartphone interaction</a
+			>
+		</li>
+		<li>
+			Then made a <a href="https://hydra-phone.pessi.info/">smartphone GUI</a> for
+			<a href="https://hydra.ojack.xyz/">Hydra</a> visual synthesizer
+		</li>
+		<li>
+			And made an <a href="https://github.com/pessi-v/qgis_openaq">OpenAirQuality plugin for QGIS</a
+			>
+		</li>
+	</ul>
+</div>
+
+<style src="../../../app.css"></style>
